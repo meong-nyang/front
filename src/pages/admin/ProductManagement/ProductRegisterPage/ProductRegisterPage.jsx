3d2c0dfa4dd243d1815677df1a3b78e8377df7d8
zxcfdsew <seonghi1313@naver.com>
2024-10-15 10:22:03 +0900

    상품등록 사진 업로드 기능 구현

--- src/pages/admin/ProductManagement/ProductRegisterPage/ProductRegisterPage.jsx
+++ src/pages/admin/ProductManagement/ProductRegisterPage/ProductRegisterPage.jsx
@@ -2,32 +2,95 @@
 import { useState } from "react";
 import MainLayout from "../../../../components/admin/MainLayout/MainLayout";
 import * as s from "./style";
+import { useMutation } from "react-query";
+import { instance } from "../../../../apis/util/instance";
 
 function ProductRegisterPage() {
 
-    const emptyRegisterData = {}
+    const emptyRegisterData = {
+        petGroupId: "",
+        categoryId: "",
+        productPrice: "",
+        productPriceDiscount: "",
+        productDetail: "",
+        productBrand: "",
+        productModel: "",
+        productMemo: "",
+        recommendation: "",
+        currentStock: "",
+        expectedStock: "",
+        productImage: ""
+    }
 
     const [ registerData, setRegisterData ] = useState(emptyRegisterData);
+    const [ selectedFiles, setSelectedFiles ] = useState([]);
+
+    const imagesToFormData = () => {
+        const formData = new FormData();
+        for (let i of selectedFiles) {
+            formData.append('productImage', i);
+        }
+        setRegisterData(data => ({
+            ...data,
+            productImage: formData
+        }));
+    }
+
+    const toFormData = () => {
+        const formData = new FormData();
+        formData.append("petGroupId", registerData.petGroupId);
+        formData.append("productPrice", registerData.productPrice);
+        formData.append("productPriceDiscount", registerData.productPriceDiscount);
+        formData.append("productDetail", registerData.productDetail);
+        formData.append("productBrand", registerData.productBrand);
+        formData.append("productModel", registerData.productModel);
+        formData.append("productMemo", registerData.productMemo);
+        formData.append("recommendation", registerData.recommendation);
+        formData.append("currentStock", registerData.currentStock);
+        formData.append("expectedStock", registerData.expectedStock);
+        for (let i of selectedFiles) {
+            formData.append('productImage', i);
+        }
+        return formData;
+    }
+
+    const registerProductMutation = useMutation(
+        async () => await instance.post("/admin/product", toFormData(), {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }),
+        {
+            onSuccess: response => {
+                console.log("등록 성공");
+                console.log(response);
+            },
+            onError: error => {
+                console.log("등록 실패");
+                console.log(error);
+            }
+        }
+    );
+
+    const handleFileOnChange = (e) => {
+        setSelectedFiles(e.target.files);
+    }
+
+    const handleRegisterButtonOnClick = () => {
+        // imagesToFormData();  // 이미지를 FormData로 변환
+        registerProductMutation.mutateAsync();
+    }
 
     return (
         <MainLayout location="상품관리 > 상품등록">
             <div css={s.layout}>
                 <div css={s.images}>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <input type="file" multiple onChange={handleFileOnChange} />
                 </div>
                 <div css={s.buttons}>
                     <span>필수 정보</span>
-                    <button>취소</button>
-                    <button>등록</button>
+                    <button onClick={() => {imagesToFormData(); console.log(registerData)}}>취소</button>
+                    <button onClick={handleRegisterButtonOnClick}>등록</button>
                 </div>
                 <div css={s.mustData}>
                     <table>
