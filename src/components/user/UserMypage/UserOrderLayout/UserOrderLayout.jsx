775f7144db8a78bbabde0e201fb142b6733ecf5e
hyunju1219 <kk020123@naver.com>
2024-10-27 21:48:56 +0900

    주문내역 디자인중

--- src/components/user/UserMypage/UserOrderLayout/UserOrderLayout.jsx
+++ src/components/user/UserMypage/UserOrderLayout/UserOrderLayout.jsx
@@ -5,7 +5,41 @@ import * as s from "./style";
 function UserOrderLayout(props) {
     return (
         <div css={s.layout}>
-            
+            <div css={s.headerLayout}>
+                <div>
+                    <p>주문일자</p>
+                    <p>2024-12-10</p>
+                </div>
+                <p>총 3건</p>
+            </div>
+
+            <div css={s.titleLayout}>
+                <p>상품명/옵션</p>
+                <p>수량</p>
+                <p>결제금액</p>
+                <p>배송상태</p>
+            </div>
+            <div css={s.contentLayout}>
+                <div css={s.productLayout}>
+                    <img src="" />
+                    <div>
+                        <p>상품이름</p>
+                        <p>옵션</p>
+                    </div>
+                </div>
+                <div>
+                    <p>상품이름</p>
+                    <p>옵션</p>
+                </div>
+                <div>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/user/UserMypage/UserOrderLayout/style.js
+++ src/components/user/UserMypage/UserOrderLayout/style.js
@@ -5,5 +5,84 @@ export const layout = css`
     width: 1000px;
     height: 500px;
     margin-top: 30px;
+    padding: 20px;
     border: 1px solid #e0e0e0;
+`;
+
+export const headerLayout = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #e0e0e0;
+    color: #777777;
+
+    & > p {
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    & > div {
+        display: flex;
+        align-items: center;
+
+        & > p:nth-of-type(2) {
+            font-size: 18px;
+            font-weight: 600;
+            margin-left: 10px;
+        }
+    }
+`;
+
+export const titleLayout = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px 0px;
+    border-bottom: 1px solid #e0e0e0;
+    & > p {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 160px;
+        &:nth-of-type(1) {
+            width: 478px;
+        }
+    }
+`;
+
+export const contentLayout = css`
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+    background-color: aliceblue;
+
+    & > div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 160px;
+        height: 100%;
+    }
+`;
+
+export const productLayout = css`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row !important;
+    width: 478px !important;
+    & > img {
+        width: 50px;
+        height: 50px;
+        background-color: red;
+    }
+    & > div {
+        display: flex;
+        flex-direction: column;
+    }
 `;
