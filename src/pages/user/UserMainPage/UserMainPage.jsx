c7196b0d9c17d940704fb61456f7112e07357296
hyunju1219 <kk020123@naver.com>
2024-11-03 20:03:34 +0900

    app 오류 수정

--- src/pages/user/UserMainPage/UserMainPage.jsx
+++ src/pages/user/UserMainPage/UserMainPage.jsx
@@ -6,12 +6,70 @@ import * as s from "./style";
 import logoImg from "../../../assets/images/logo.png";
 import { RiNotionFill } from "react-icons/ri";
 import { BiLogoGithub } from "react-icons/bi";
-
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import {
+    SliderContainer,
+    BodyContainer,
+    SliderContent,
+  } from './style'
 function UserMainPage(props) {
+
+    const list = [
+        {
+          content: 1,
+          color: '#FF5757',
+        },
+        {
+          content: 2,
+          color: '#FFBC57',
+        },
+        {
+          content: 3,
+          color: '#FFEE57',
+        },
+        {
+          content: 4,
+          color: '#57FF86',
+        },
+        {
+          content: 5,
+          color: '#5786FF',
+        },
+        {
+          content: 6,
+          color: '#8013D7',
+        },
+      ]
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
+
     return (
         <UserBackgoundLayout>
             <div css={s.layout}>
-
+                <header>
+                    <BodyContainer>
+                        <SliderContainer>
+                            <Slider {...settings}>
+                            {list.map((value, index) => (
+                                <div>
+                                <SliderContent
+                                    $color={value.color}
+                                    key={index}>
+                                    {value.content}
+                                </SliderContent>
+                                </div>
+                            ))}
+                            </Slider>
+                        </SliderContainer>
+                    </BodyContainer>
+                </header>
                 <footer css={s.footerLayout}>
                     <div>
                         <div css={s.infoLayout}>
